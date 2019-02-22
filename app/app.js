ef57a40adf2a7749d83b9d68ec090737d98d94d7
Lance Reck <lance.reck@down-route.com>
2019-02-21 21:51:11 -0800

    Removed autoupdate

--- app/app.js
+++ app/app.js
@@ -54,4 +54,4 @@ app.run(['editableOptions', function (editableOptions) {
 }]);
 
 //electron autoupdate
-app.run(['common', 'dialogs', 'electron', 'VERSION', require('./app/config.autoupdate')]);
+// app.run(['common', 'dialogs', 'electron', 'VERSION', require('./app/config.autoupdate')]);
